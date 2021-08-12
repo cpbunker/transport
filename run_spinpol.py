7f076a2e5d98ff0d4174f2c0359a1a20f769ba35
Christian Bunker <christian@bunkerplanet.com>
2021-08-12 14:28:42 -0400

    More data as I try to identify why Vg=-tsqrt(K-1) is a special case

--- run_spinpol.py
+++ run_spinpol.py
@@ -16,26 +16,26 @@ import matplotlib.pyplot as plt
 #### prepare dot in diff spin states
 
 # top level
-verbose = 5;
-nleads = (3,3);
+verbose = 3;
+nleads = (2,2);
 nelecs = (sum(nleads)+1,0); # half filling
-get_data = False; # whether to run computations, if not data already exists
+get_data = True; # whether to run computations, if not data already exists
 
 # phys params, must be floats
 tl = 1.0;
 th = 0.1; # can scale down and same effects are seen. Make sure to do later
 Vb = -0.5;
 mu = 0.0;
-Vg = -4.0;
-U = 8.0
+Vg = -2.0;
+U = 10.0;
 Bs = [tl*5, tl*5, tl*5,tl*5,tl*5];
 thetas = [0.0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi];
 Rlead_pol = 1
-assert( Vg == -0.5*U);
+#assert( Vg == -0.5*U);
 
 #time info
 dt = 0.04;
-tf = 3.0;
+tf = 1.0;
 
 datafs = [];
 if get_data: # must actually compute data
@@ -44,9 +44,8 @@ if get_data: # must actually compute data
         B, theta = Bs[i], thetas[i];
         params = tl, th, Vb, mu, Vg, U, B, theta;
         fname = siam_current.DotData(nleads, nelecs, tf, dt, phys_params=params, Rlead_pol = Rlead_pol, prefix = "spinpolsweep/", verbose = verbose);
-        datafs.append(fname);
 
-else: # already there
+if True: # already there
     splots = ['Jtot','occ','delta_occ','Sz','delta_Sz',]; # which subplots to plot
     for i in range(len(Bs)):
         datafs.append("dat/DotData/spinpolsweep/"+str(nleads[0])+"_1_"+str(nleads[1])+"_e"+str(sum(nelecs))+"_B"+str(Bs[i])+"_t"+str(thetas[i])[:3]+"_Vg"+str(Vg)+".npy");
