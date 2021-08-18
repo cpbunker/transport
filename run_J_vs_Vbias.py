47d9442e957a6e46c5670309a138e75eedcd54d0
Christian Bunker <christian@bunkerplanet.com>
2021-08-18 11:31:53 -0400

    sweep Vbias on nersc

--- run_J_vs_Vbias.py
+++ run_J_vs_Vbias.py
@@ -4,7 +4,6 @@ M^2QM at UF
 August 2021
 
 Runner file for prepping dot spin state with B field, getting current output
-Now assuming polarizer btwn Rlead, dot
 '''
 
 import siam_current
@@ -18,15 +17,15 @@ import numpy as np
 verbose = 3;
 nleads = (2,2);
 nelecs = (sum(nleads)+1,0); # half filling
-get_data = False; # whether to run computations, if not data already exists
+get_data = True; # whether to run computations, if not data already exists
 
 # phys params, must be floats
 tl = 1.0;
 th = tl/10; # can scale down and same effects are seen. Make sure to do later
-Vb = -1/100*tl;
+Vbs = [-tl/100, -2*tl/10, -tl/10,-tl]
 mu = 10.0*tl;
-Vgs = [0.0,5.0,7.5,10.0,12.5]
-U = 100*tl;
+Vg = mu
+Us = 100*tl;
 B = 5*tl;
 theta = 0.0;
 phi = 0.0;
@@ -37,8 +36,8 @@ tf = 5.0;
 
 if get_data: # must actually compute data
 
-    for i in range(len(Vgs)): # iter over Vg vals;
-        Vg = Vgs[i];
+    for i in range(len(Vbs)): # iter over Vg vals;
+        Vb = Vbs[i]
         params = tl, th, Vb, mu, Vg, U, B, theta, phi;
         siam_current.DotData(nleads, nelecs, tf, dt, prefix = "", phys_params=params, verbose = verbose);
 
@@ -51,10 +50,10 @@ else:
     labs = [];
     splots = ['Jtot','J']; # which subplots to plot
 
-    for i in range(len(Vgs)):
-        Vg = Vgs[i];
-        datafs.append("dat/mu10/fci_"+str(nleads[0])+"_1_"+str(nleads[1])+"_e"+str(sum(nelecs))+"_B"+str(B)[:3]+"_t"+str(theta)[:3]+"_Vg"+str(Vg)+".npy");
-        labs.append("Vg = "+str(Vg) );
+    for i in range(len(Us)):
+        Vb = Vbs[i];
+        datafs.append("dat/Vbias/fci_"+str(nleads[0])+"_1_"+str(nleads[1])+"_e"+str(sum(nelecs))+"_B"+str(B)[:3]+"_t"+str(theta)[:3]+"_Vb"+str(Vb)+".npy");
+        labs.append("Vb = "+str(Vb) );
     
     plot.CompObservables(datafs, nleads, Vg, labs, whichi = 0, splots = splots);
 
