11be61e48711235d95185d7a37f61201e1bef64d
cpbunker <christian@bunkerplanet.com>
2021-09-04 12:06:29 -0700

    Decently replicate peng's results in dat/peng for 414 dmrg system G respectable for th=0.1 (scales right to th=0.4)

--- run_J_414_dmrg.py
+++ run_J_414_dmrg.py
@@ -1,10 +1,8 @@
 '''
 Christian Bunker
 M^2QM at UF
-September 2021
+August 2021
 
-Recreate sequential tunneling (ST) results in Recher's "quantum dot paper as spin filter" paper
-Should see large current when energy of singlet state ~ mu
 '''
 
 import siam_current
@@ -14,7 +12,7 @@ import numpy as np
 import sys
 
 ##################################################################################
-#### set up physics of dot
+#### 
 
 # top level
 verbose = 3;
@@ -24,24 +22,28 @@ get_data = int(sys.argv[1]); # whether to run computations, if not data already 
 
 # phys params, must be floats
 tl = 1.0;
-th = 0.1; 
-Vb = 10.0;
-mu = 1.0;
-Vgs = [-10.0];
-U =  20.0;
-B = 1.0*tl;
+th = 1/10; # can scale down and same effects are seen. Make sure to do later
+Vb = 1/100;
+mu = 0.1;
+Vgs = [-1.0];
+U = 2.0
+B = 0.0;
 theta = 0.0;
 
 #time info
-dt = 0.04;
-tf = 1.0;
+dt = 0.1;
+tf = 5.0;
+
+# dmrg info
+bdims = [300, 400, 450, 500];
+noises = [1e-3, 1e-4, 1e-5, 0];
 
 if get_data: # must actually compute data
 
     for i in range(len(Vgs)): # iter over Vg vals;
         Vg = Vgs[i];
         params = tl, th, Vb, mu, Vg, U, B, theta;
-        siam_current.DotData(nleads, nelecs, tf, dt, params, prefix = "", verbose = verbose);
+        siam_current.DotDataDmrg(nleads, nelecs, tf, dt, params, bdims, noises, prefix = "", verbose = verbose);
 
 else:
 
@@ -50,9 +52,9 @@ else:
     # plot results
     datafs = sys.argv[2:]
     labs = Vgs # one label for each Vg
-    splots = ['Jup','Jdown','delta_occ','Sz','Szleads','E']; # which subplots to plot
+    splots = ['Jup','Jdown','occ','Sz']; # which subplots to plot
     title = "Current between impurity and left, right leads"
-    plot.CompObservables(datafs, labs, splots = splots, mytitle = title, leg_title = "$V_g$", leg_ncol = 1, whichi = 0);
+    plot.CompObservables(datafs, labs, splots = splots, mytitle = title, leg_title = "$V_g$", leg_ncol = 3);
 
     
 
--- run_J_dmrg.py
+++ run_J_dmrg.py
@@ -1,10 +1,10 @@
 '''
 Christian Bunker
 M^2QM at UF
-September 2021
+August 2021
 
-Recreate sequential tunneling (ST) results in Recher's "quantum dot paper as spin filter" paper
-Should see large current when energy of singlet state ~ mu
+Runner file for prepping dot spin state with B field, getting current output
+Now assuming polarizer btwn Rlead, dot
 '''
 
 import siam_current
@@ -14,40 +14,31 @@ import numpy as np
 import sys
 
 ##################################################################################
-#### set up physics of dot
+#### 
 
 # top level
 verbose = 3;
-nleads = (4,4);
+nleads = (8,7);
 nelecs = (sum(nleads)+1,0); # half filling
 get_data = int(sys.argv[1]); # whether to run computations, if not data already exists
 
 # phys params, must be floats
 tl = 1.0;
 th = 0.1; # can scale down and same effects are seen. Make sure to do later
-Vb = 10.0;
-mu = 1.0;
-Vgs = [-10.0];
-U =  20.0;
-B = 1.0;
+Vb = 0.01;
+mu = 0.1;
+Vgs = [-1.0];
+U = 2.0
+B = 0.0;
 theta = 0.0;
 
 #time info
-dt = 0.01;
-tf = 0.5;
+dt = 0.1;
+tf = 5.0;
 
-# dmrg
-if( nleads == (4,4) ):
-    bdims = [300, 400, 450, 500];
-    noises = [1e-3, 1e-4, 1e-5, 0.0];
-elif( nleads == (8,7) ):
-    bdims = [1400,1600,1800,2000];
-    noises = [1e-3, 1e-4, 1e-5,0.0];
-elif( nleads == (10,10)):
-    bdims = [1400,1600,1800,2000];
-    noises = [1e-3, 1e-4, 1e-5,0.0];
-else:
-    assert(False);
+# dmrg info
+bdims = [1400,1600,1800,2000];
+noises = [1e-3, 1e-4, 1e-5, 0];
 
 if get_data: # must actually compute data
 
@@ -63,9 +54,9 @@ else:
     # plot results
     datafs = sys.argv[2:]
     labs = Vgs # one label for each Vg
-    splots = ['Jup','Jdown','occ','delta_occ','Sz','Szleads','E']; # which subplots to plot
+    splots = ['Jup','Jdown','occ','Sz']; # which subplots to plot
     title = "Current between impurity and left, right leads"
-    plot.CompObservables(datafs, labs, splots = splots, mytitle = title, leg_title = "$V_g$", leg_ncol = 1, whichi = 0);
+    plot.CompObservables(datafs, labs, splots = splots, mytitle = title, leg_title = "$V_g$", leg_ncol = 3);
 
     
 
