Major bug fix: sign of tl
Checked, results same

run_cicc.py
@@ -75,7 +75,7 @@ if False:
         hmats, tmats = wfm.h_cicc_eff(Jeff, tl, i1, i2, Nsites);
 
         # get T from this setup
-        Tvals.append(list(wfm.Tcoef(hmats, tmats, E_rho , source)) );
+        Tvals.append(list(wfm.Tcoef(hmats, tmats, tl, E_rho , source)) );
 
     # package into one array
     Tvals = np.array(Tvals);
@@ -95,7 +95,7 @@ if True: # plot
     # plot each file given at command line
     fig, axes = plt.subplots();
     axes = [axes];
-    datafs = sys.argv[2:];
+    datafs = sys.argv[1:];
     for fi in range(len(datafs)):
 
         # unpack
@@ -115,7 +115,7 @@ if True: # plot
     axes[0].axvline(2*np.pi, color = "black", linestyle = "dashed");
     axes[0].set_ylim(0.0,1.05);
     axes[0].set_xlabel("$kx_{0}$");
-    axes[0].set_title("Up electron scattering from down, down impurities, J = "+str(np.real(Jeff)) );
+    axes[0].set_title("Up electron scattering from $\Psi^+ $ impurities, J = "+str(np.real(Jeff)) );
     axes[0].set_ylabel("$T$");
     for ax in axes:
         ax.minorticks_on();
@@ -170,7 +170,7 @@ if False:
         hmats, tmats = wfm.h_cicc_eff(Jeff, tl, i1, i2, Nsites);
 
         # get T from this setup
-        Tvals.append(list(wfm.Tcoef(hmats, tmats, E_rho , sourcei)) );
+        Tvals.append(list(wfm.Tcoef(hmats, tmats, tl, E_rho , sourcei)) );
 
     # save results
     Tvals = np.array(Tvals); # rows are diff rhoJ vals

run_cicc_nersc.py
@@ -27,7 +27,7 @@ verbose = 5
 #### effective ham, choose source for initial condition
 #### ie replicate Cicc figs 1, 5
     
-for rJ in [10.0]:
+for rJ in [1.0]:
 
     # siam inputs
     tl = 1.0;
@@ -48,13 +48,13 @@ for rJ in [10.0]:
     E_rho = E_rho - 2*tl; # measure from mu
 
     # choose boundary condition
-    source = np.array([0,1,1,0,0,0,0,0])/np.sqrt(2);
+    source = np.array([0,1,-1,0,0,0,0,0])/np.sqrt(2);
     #source = source/np.dot(source, source); # normalize
     
     
     # mesh of x0s (= N0s * alat)
     kx0min, kx0max = 3*np.pi/4, 1.1*np.pi;
-    #kx0min, kx0max = 0.0, 2.1*np.pi;
+    kx0min, kx0max = 0.0, 1.1*np.pi;
     N0min, N0max = int(kx0min/(k_rho*alat)), int(kx0max/(k_rho*alat));
     if verbose: print("N0 min, max = ",N0min, N0max);
     N0vals = np.linspace(N0min, N0max, int(N0max - N0min), dtype = int); # always integer
@@ -72,9 +72,10 @@ for rJ in [10.0]:
         Nsites = i2+2; # 1 lead site on each side
         print("i1, i2, Nsites = ",i1, i2, Nsites)
         hmats, tmats = wfm.h_cicc_eff(Jeff, tl, i1, i2, Nsites);
+        if(N0i == 1): print(hmats, "\n", tmats);
 
         # get T from this setup
-        Tvals.append(list(wfm.Tcoef(hmats, tmats, E_rho , source, verbose = 1)) );
+        Tvals.append(list(wfm.Tcoef(hmats, tmats, tl, E_rho , source, verbose = 1)) );
 
     # package into one array
     Tvals = np.array(Tvals);

run_menez.py
@@ -48,10 +48,11 @@ if False: # plot at different eff J
         if verbose: print(h_menez,"\n", tl_arr);
 
         # define source, ie what is incident at left bdy, in this case an up electron
-        sourcei = 1; # up e, down imp
+        source = np.zeros(np.shape(h_SR)[0]);
+        source[2] = 1;
 
         if False: # test at max verbosity
-            myT = wfm.Tcoef(h_menez, tl_arr, -1.99, sourcei, verbose = 5);
+            myT = wfm.Tcoef(h_menez, tl_arr, tl, -1.99, source, verbose = 5);
             if verbose: print("******",myT);
 
         # sweep over range of energies
@@ -64,7 +65,7 @@ if False: # plot at different eff J
 
         # sweep thru E
         for Ei in range(len(Evals) ):
-            dummyT1, Tupvals[Ei], Tdownvals[Ei], dummyT2 = wfm.Tcoef(h_menez, tl_arr, Evals[Ei], sourcei);
+            dummyT1, Tupvals[Ei], Tdownvals[Ei], dummyT2 = wfm.Tcoef(h_menez, tl_arr, tl, Evals[Ei], source);
             assert(dummyT1 == 0 and dummyT2 == 0);
 
         # plot
@@ -129,10 +130,11 @@ if True: # use real ham instead of eff
     if verbose: print(h_menez,"\n", tl_arr);
 
     # define source, ie what is incident at left bdy, in this case an up electron
-    sourcei = 2;
+    source = np.zeros(np.shape(h_SR)[0]);
+    source[2] = 1;
 
     if False: # test at max verbosity
-        myT = wfm.Tcoef(h_menez, tl_arr, -1.99, sourcei, verbose = 5);
+        myT = wfm.Tcoef(h_menez, tl_arr, tl, -1.99, source, verbose = 5);
         if verbose: print("******",myT);
 
     # sweep over range of energies
@@ -142,12 +144,12 @@ if True: # use real ham instead of eff
     Evals = np.linspace(Emin, Emax, N, dtype = complex);
     Tvals = [];
     for Ei in range(len(Evals) ):
-        Tvals.append(wfm.Tcoef(h_menez, tl_arr, Evals[Ei], sourcei));
+        Tvals.append(wfm.Tcoef(h_menez, tl_arr, tl, Evals[Ei], source));
 
     # plot Tvals vs E
     Tvals = np.array(Tvals);
     fig, ax = plt.subplots();
-    ax.scatter(Evals + 2*tl,Tvals[:,sourcei+1], marker = 's',label = "$T_{down}$");
+    ax.scatter(Evals + 2*tl,Tvals[:,3], marker = 's',label = "$T_{down}$");
 
     # menezes prediction in the continuous case
     # all the definitions, vectorized funcs of E

run_switzer.py
@@ -23,27 +23,30 @@ np.set_printoptions(precision = 4, suppress = True);
 verbose = 5
 
 # define source
-sourcei = 0; # corresponds to |up, 2, 1 > in the device basis
+source = np.zeros(8); # corresponds to |up, 2, 1 > in the device basis
+source[0] = 1;
 
 # define params according to Eric's paper
 tl = 1.0; # hopping
-D = 0.2;
-JH = 0.3;
-JK2 = 0.1;
-JK3 = 0.1;
+D = -0.06;
+JH = -0.005;
+JK2 = -0.04;
+JK3 = -0.04;
 
-if True: # iter over tl
+if True: 
 
     # eff params at resonance
     JK = D*2/3;
     DeltaK = 0;
 
-    tlvals = np.linspace(0.5,9.5,20);
+    # iter over N
+    Nmax = 30
+    Nvals = np.linspace(1,Nmax,Nmax,dtype = int);
     Tvals = [];
-    for mytl in tlvals:
+    for N in Nvals:
 
         # fix energy near bottom of band
-        Energy = (-2)*mytl + 0.1;
+        Energy = -2*tl + 0.5;
 
         # eff exchange in SR
         h_ex = np.array([[0,DeltaK,4*JK,              0,0,0,0,0],   # block diag m=3/2
@@ -55,24 +58,37 @@ if True: # iter over tl
                          [0,0,0,         0,0,0,0,0],
                          [0,0,0,         0,0,0,0,0]]);
 
-        # package as block hams
-        hblocks = np.array([np.zeros_like(h_ex), h_ex, np.zeros_like(h_ex)]);
-        tblocks = np.array([-mytl*np.eye(*np.shape(h_ex)), -mytl*np.eye(*np.shape(h_ex))]);
+        # package as block hams 
+        # number of blocks depends on N
+        hblocks = [np.zeros_like(h_ex)]
+        tblocks = [-tl*np.eye(*np.shape(h_ex)) ];
+        for Ni in range(N):
+            hblocks.append(np.copy(h_ex));
+            tblocks.append(-tl*np.eye(*np.shape(h_ex)) );
+        hblocks.append(np.zeros_like(h_ex) );
+        hblocks = np.array(hblocks);
+        tblocks = np.array(tblocks);
+        if (N==2): print(hblocks, "\n", tblocks);
 
         # coefs
-        Tvals.append(wfm.Tcoef(hblocks, tblocks, Energy, sourcei));
+        Tvals.append(wfm.Tcoef(hblocks, tblocks, tl, Energy, source));
 
     # plot
     Tvals = np.array(Tvals);
     fig, ax = plt.subplots();
-    ax.scatter(tlvals, Tvals[:,0], marker = 's', label = '|up, 2, 1>');
-    ax.scatter(tlvals, Tvals[:,1], marker = 's', label = '|up, 1, 1>');
-    ax.scatter(tlvals, Tvals[:,2], marker = 's', label = '|down, 2, 1>');
+    if False:
+        ax.scatter(Nvals, Tvals[:,0], marker = 's', label = '|up, 2, 1>');
+        ax.scatter(Nvals, Tvals[:,1], marker = 's', label = '|up, 1, 1>');
+        ax.scatter(Nvals, Tvals[:,2], marker = 's', label = '|down, 2, 1>');
+    else:
+        ax.plot(Nvals, Tvals[:,0], label = '|up, 2, 1>');
+        ax.plot(Nvals, Tvals[:,1], label = '|up, 1, 1>');
+        ax.plot(Nvals, Tvals[:,2], label = '|down, 2, 1>');
 
     # format
-    ax.set_title("");
+    ax.set_title("Transmission at resonance, $J_K = 2D/3$");
     ax.set_ylabel("$T$");
-    ax.set_xlabel("$E + 2*t_l$");
+    ax.set_xlabel("$N$");
     ax.set_ylim(0.0,1.05);
     plt.legend();
     ax.minorticks_on();
@@ -80,66 +96,16 @@ if True: # iter over tl
     ax.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5);
     plt.show();
 
-if False: # iter over E
+if False: # iter over th, ie hopping in/out of SR
 
-    # eff params
-    JK = (JK2 + JK3)/2;
-    DeltaK = JK2 - JK3;
-
-    # eff exchange in SR
-    h_ex = np.array([[0,DeltaK,4*JK,              0,0,0,0,0],   # block diag m=3/2
-                     [DeltaK,-8*JH,-2*DeltaK,     0,0,0,0,0],
-                     [4*JK,-2*DeltaK,-6*JK+4*D,   0,0,0,0,0],
-                     [0,0,0,         0,0,0,0,0],   # block diag m=1/2
-                     [0,0,0,         0,0,0,0,0],
-                     [0,0,0,         0,0,0,0,0],
-                     [0,0,0,         0,0,0,0,0],
-                     [0,0,0,         0,0,0,0,0]]);
-
-    # package as block hams
-    hblocks = np.array([np.zeros_like(h_ex), h_ex, np.zeros_like(h_ex)]);
-    tblocks = np.array([-tl*np.eye(*np.shape(h_ex)), -tl*np.eye(*np.shape(h_ex))]);
-
-    # iter over E
-    Emin, Emax = -1.99999, -2.0 + 0.9
-    N = 40;
-    Evals = np.linspace(Emin, Emax, N, dtype = complex);
-    Tvals = [];
-    for Ei in range(len(Evals) ):
-        Tvals.append(wfm.Tcoef(hblocks, tblocks, Evals[Ei], sourcei));
-
-    # plot
-    Tvals = np.array(Tvals);
-    fig, ax = plt.subplots();
-    ax.scatter(Evals+2*tl, Tvals[:,0], marker = 's', label = '|up, 2, 1>');
-    ax.scatter(Evals+2*tl, Tvals[:,1], marker = 's', label = '|up, 1, 1>');
-    ax.scatter(Evals+2*tl, Tvals[:,2], marker = 's', label = '|down, 2, 1>');
-
-    # format
-    ax.set_title("");
-    ax.set_ylabel("$T$");
-    ax.set_xlabel("$E + 2*t_l$");
-    ax.set_ylim(0.0,1.05);
-    plt.legend();
-    ax.minorticks_on();
-    ax.grid(which='major', color='#DDDDDD', linewidth=0.8);
-    ax.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5);
-    plt.show();
-
-if False: # iter over J
-
-    # fix energy near bottom of band
-    Energy = -2*tl + 0.1;
+    # eff params at resonance
+    JK = D*2/3;
+    DeltaK = 0;
     
     # iter over JK looking for resonance
-    jkvals = np.linspace(0.0,2.0,20);
+    thvals = np.linspace(0.1,0.9,40);
     Tvals = []; # get transmission each time
-    for jkval in jkvals:
-
-        # eff params
-        JK2, JK3 = jkval, jkval;
-        JK = (JK2 + JK3)/2;
-        DeltaK = JK2 - JK3;
+    for th in thvals:
 
         # eff exchange in SR
         h_ex = np.array([[0,DeltaK,4*JK,              0,0,0,0,0],   # block diag m=3/2
@@ -153,22 +119,70 @@ if False: # iter over J
 
         # package as block hams
         hblocks = np.array([np.zeros_like(h_ex), h_ex, np.zeros_like(h_ex)]);
+        tblocks = np.array([-th*np.eye(*np.shape(h_ex)), -th*np.eye(*np.shape(h_ex))]);
+
+        # get transmission coefs
+        Energy = -2*tl + 2; # fix
+        Tvals.append(wfm.Tcoef(hblocks, tblocks, tl, Energy, source));
+
+    # plot
+    Tvals = np.array(Tvals);
+    fig, ax = plt.subplots();
+    ax.scatter(thvals, Tvals[:,0], marker = 's', label = '|up, 2, 1>');
+    ax.scatter(thvals, Tvals[:,1], marker = 's', label = '|up, 1, 1>');
+    ax.scatter(thvals, Tvals[:,2], marker = 's', label = '|down, 2, 1>');
+
+    # format
+    ax.set_title("Transmission at resonance, $J_K = 2D/3$");
+    ax.set_ylabel("$T$");
+    ax.set_xlabel("$t_h$");
+    ax.set_ylim(0.0,1.05);
+    plt.legend();
+    ax.minorticks_on();
+    ax.grid(which='major', color='#DDDDDD', linewidth=0.8);
+    ax.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5);
+    plt.show();
+
+if True: 
+
+    # eff params at resonance
+    JK = D*2/3;
+    DeltaK = 0;
+    
+    # modulate
+    Vgvals = np.linspace(0.0,0.5,40);
+    Tvals = []; # get transmission each time
+    for Vg in Vgvals:
+
+        # eff exchange in SR
+        h_ex = np.array([[0,DeltaK,4*JK,              0,0,0,0,0],   # block diag m=3/2
+                         [DeltaK,-8*JH,-2*DeltaK,     0,0,0,0,0],
+                         [4*JK,-2*DeltaK,-6*JK+4*D,   0,0,0,0,0],
+                         [0,0,0,         0,0,0,0,0],   # block diag m=1/2
+                         [0,0,0,         0,0,0,0,0],
+                         [0,0,0,         0,0,0,0,0],
+                         [0,0,0,         0,0,0,0,0],
+                         [0,0,0,         0,0,0,0,0]]);
+
+        # package as block hams
+        hblocks = np.array([np.zeros_like(h_ex), h_ex-np.eye(*np.shape(h_ex))*Vg/2, np.zeros_like(h_ex)-np.eye(*np.shape(h_ex))*Vg]);
         tblocks = np.array([-tl*np.eye(*np.shape(h_ex)), -tl*np.eye(*np.shape(h_ex))]);
 
         # get transmission coefs
-        Tvals.append(wfm.Tcoef(hblocks, tblocks, Energy, sourcei));
+        Energy = -2*tl + 0.5; # fix
+        Tvals.append(wfm.Tcoef(hblocks, tblocks, tl, Energy, source));
 
     # plot
     Tvals = np.array(Tvals);
     fig, ax = plt.subplots();
-    ax.scatter(jkvals, Tvals[:,0], marker = 's', label = '|up, 2, 1>');
-    ax.scatter(jkvals, Tvals[:,1], marker = 's', label = '|up, 1, 1>');
-    ax.scatter(jkvals, Tvals[:,2], marker = 's', label = '|down, 2, 1>');
+    ax.scatter(Vgvals, Tvals[:,0], marker = 's', label = '|up, 2, 1>');
+    ax.scatter(Vgvals, Tvals[:,1], marker = 's', label = '|up, 1, 1>');
+    ax.scatter(Vgvals, Tvals[:,2], marker = 's', label = '|down, 2, 1>');
 
     # format
-    ax.set_title("");
+    ax.set_title("Transmission at resonance, $J_K = 2D/3$");
     ax.set_ylabel("$T$");
-    ax.set_xlabel("$J_K$");
+    ax.set_xlabel("$V_g$");
     ax.set_ylim(0.0,1.05);
     plt.legend();
     ax.minorticks_on();

wfm.py
@@ -58,19 +58,19 @@ def Hmat(h, t, verbose = 0):
     return H; 
 
 
-def Hprime(h, t, E, verbose = 0):
+def Hprime(h, t, tl, E, verbose = 0):
     '''
     Make H' (hamiltonian + self energy) for N+2 x N+2 system
     where there are N sites in the scattering region (SR).
 
-    h, 1d arr, length N+2, on site energies
-    t, float, hopping
+    h, block diag hamiltonian matrices
+    t, block off diag hopping matrix
+    tl, hopping in leads, not necessarily same as hopping on/off SR as def'd by t matrices
     '''
 
     # unpack
     N = len(h) - 2; # num scattering region sites
     n_loc_dof = np.shape(h[0])[0];
-    tval = t[0][0,0];
 
     # add self energies to hamiltonian
     Hp = Hmat(h, t, verbose = verbose); # regular ham
@@ -79,24 +79,24 @@ def Hprime(h, t, E, verbose = 0):
     # need a self energy for each LL boundary condition
     for Vi in range(n_loc_dof): # iters over all bcs
         V = h[0][Vi,Vi];
-        lamL = (E-V)/(-2*tval);
+        lamL = (E-V)/(-2*tl);
         LambdaLminus = lamL - np.lib.scimath.sqrt(lamL*lamL - 1); # incident
-        SigmaL = -tval/LambdaLminus;
+        SigmaL = -tl/LambdaLminus;
         Hp[Vi,Vi] = SigmaL;
 
     # self energies at RL
     for Vi in range(n_loc_dof): # iters over all bcs
         V = h[-1][Vi,Vi];     
-        lamR = (E-V)/(-2*tval);
+        lamR = (E-V)/(-2*tl);
         LambdaRplus = lamR + np.lib.scimath.sqrt(lamR*lamR - 1); # transmitted wavevector
-        SigmaR = -tval*LambdaRplus;
+        SigmaR = -tl*LambdaRplus;
         Hp[Vi-n_loc_dof,Vi-n_loc_dof] = SigmaL;
     
     if verbose > 3: print("\nH' = \n",Hp);
     return Hp;
 
 
-def Green(h, t, E, verbose = 0):
+def Green(h, t, tl, E, verbose = 0):
     '''
     Greens function for system described by
     - potential V[i] at site i
@@ -113,21 +113,21 @@ def Green(h, t, E, verbose = 0):
     # unpack
     N = len(h) - 2; # num scattering region sites
     n_loc_dof = np.shape(h[0])[0];
-    tval = t[0][0,0];
 
     # get green's function matrix
-    Hp = Hprime(h, t, E, verbose = verbose);
+    Hp = Hprime(h, t, tl, E, verbose = verbose);
     G = np.linalg.inv( E*np.eye(np.shape(Hp)[0] ) - Hp );
 
     # of interest is the qith row which contracts with the source q
     return G;
 
 
-def Tcoef(h, t, E, qi, verbose = 0):
+def Tcoef(h, t, tl, E, qi, verbose = 0):
     '''
     coefficient for a transmitted up and down electron
     h, block diag hamiltonian matrices
-    t, off diag hopping matrix
+    t, block off diag hopping matrix
+    tl, hopping in leads, not necessarily same as hopping on/off SR as def'd by t matrices
     E, energy of the incident electron
     qi, source vector (loc dof only)
     '''
@@ -141,30 +141,30 @@ def Tcoef(h, t, E, qi, verbose = 0):
     # unpack
     N = len(h) - 2; # num scattering region sites
     n_loc_dof = np.shape(h[0])[0];
-    tval = t[0][0,0];
 
     # self energies at LL
     # need a self energy for each LL boundary condition
     SigmaL = [];
     for Vi in range(n_loc_dof): # iters over all bcs
         V = h[0][Vi,Vi];
-        lamL = (E-V)/(-2*tval);
+        lamL = (E-V)/(-2*tl);
         LambdaLminus = lamL - np.lib.scimath.sqrt(lamL*lamL - 1); # incident
-        SigmaL.append( -tval/LambdaLminus);
+        SigmaL.append( -tl/LambdaLminus);
 
     # self energies at RL
     SigmaR = [];
     for Vi in range(n_loc_dof): # iters over all bcs
         V = h[-1][Vi,Vi];     
-        lamR = (E-V)/(-2*tval);
+        lamR = (E-V)/(-2*tl);
         LambdaRplus = lamR + np.lib.scimath.sqrt(lamR*lamR - 1); # transmitted wavevector
-        SigmaR.append( -tval*LambdaRplus);
+        SigmaR.append( -tl*LambdaRplus);
 
     # check self energies
+    print(">>>>",tl,E-V,SigmaR[0]);
     assert( isinstance(SigmaL[0], complex) and isinstance(SigmaR[0], complex)); # check right dtype
 
     # green's function
-    G = Green(h, t, E, verbose = verbose);
+    G = Green(h, t, tl, E, verbose = verbose);
     if verbose > 3: print("\nG[:,qi] = ",G[:,qi]);
 
     # coefs
@@ -243,7 +243,7 @@ def h_cicc_eff(J, t, i1, i2, Nsites):
     # hopping connects like spin orientations only, ie is identity
     tl_arr = []
     for sitei in range(Nsites-1):
-        tl_arr.append(t*np.eye(*np.shape(Se_dot_S1)) );
+        tl_arr.append(-t*np.eye(*np.shape(Se_dot_S1)) );
     tl_arr = np.array(tl_arr);
 
     return h_cicc, tl_arr;