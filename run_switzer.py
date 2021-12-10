8295b39459d167d01f527de57e7a35e5c26cdd42
Christian Bunker <christian@bunkerplanet.com>
2021-12-10 11:48:47 -0500

    Clean up run_swirzer, verified works. Commit before big changes

--- run_switzer.py
+++ run_switzer.py
@@ -15,241 +15,107 @@ from transport.wfm import utils
 import numpy as np
 import matplotlib.pyplot as plt
 
-import sys
+import itertools
 
 # top level
 plt.style.use("seaborn-dark-palette");
 np.set_printoptions(precision = 4, suppress = True);
 verbose = 5;
-option = sys.argv[1];
 
-# define params according to Eric's paper
+# tight binding params
 tl = 1.0; # hopping >> other params
+
+# Eric's params
 D = -0.06
 JH = -0.005;
+JK1 = -0.04;
+JK2 = -0.04;
+JK = (JK1 + JK2)/2;
+DeltaK = JK1 - JK2;
 
-# eff params at resonance
-JK = 2*D/3;
-DeltaK = 0;
+# def particles and their single particle states
+species = np.array([1,1,1]); # num of each species, which are one e, elec, spin-3/2, spin-3/2
+spec_strs = ["e","1","2"];
+states = [[0,1],[2,3,4],[5,6,7]]; # e up, down, spin 1 mz, spin 2 mz
+state_strs = ["0.5_","-0.5_","1.0_","0.0_","-1.0_","1.0_","0.0_","-1.0_"];
+dets = np.array([xi for xi in itertools.product(*tuple(states))]); # product states
+dets32 = [[0,2,6],[0,3,5],[1,2,5]]; # total spin 3/2 subspace
 
-#### different ways of doing the scattering region
+# source
+sourcei = 9; #| down, 1, 1 > = [1 2 5]
+source = np.zeros(len(dets));
+source[sourcei] = 1;
+source_str = "|";
+for si in dets[sourcei]: source_str += state_strs[si];
+source_str += ">";
+print("\nSource:\n"+source_str);
 
-if option == "direct": # directly write down ham --> rabi flopping at resonance
+# entangled pair
+pair = (1,3); #|up, 1, 0> = [0 2 6] and |up,0,1> = [0,3,5]
+if(verbose):
+    print("\nEntangled pair:");
+    pair_strs = [];
+    for pi in pair:
+        pair_str = "|";
+        for si in dets[pi]: pair_str += state_strs[si];
+        pair_str += ">";
+        print(pair_str);
+        pair_strs.append(pair_str);
 
-    # define source
-    source = np.zeros(3);
-    source[2] = 1; # me, m1, m2 = down, 1, 1 state
-    
-    # fix energy near bottom of band
-    Energy = -2*tl + 0.5;
-    ka = np.arccos(Energy/(-2*tl));
-    #print("ka = ", ka);
-    #print("vg = ", 2*tl*np.sin(ka));
+# fix energy near bottom of band
+Energy = -2*tl + 0.5;
+ka = np.arccos(Energy/(-2*tl));
 
-    # iter over N
-    Nmax = 80
-    Nvals = np.linspace(1,Nmax,min(Nmax, 20),dtype = int);
-    Tvals = [];
-    for N in Nvals:
+# 2nd qu'd ham
+h1e, g2e = ops.h_switzer(D, JH, JK1, JK2);
 
-        # eff exchange in SR
-        h_ex = (1/4)*np.array([[0,DeltaK,4*JK],   # block diag m=3/2 subspace
-                         [DeltaK,-8*JH,-2*DeltaK],
-                         [4*JK,-2*DeltaK,-6*JK+4*D] ]);
+# convert to many body form
+h_SR = fci_mod.single_to_det(h1e,g2e, species, states);
 
-        # package as block hams 
-        # number of blocks depends on N
-        hblocks = [np.zeros_like(h_ex)]
-        tblocks = [-tl*np.eye(*np.shape(h_ex)) ];
-        for Ni in range(N):
-            hblocks.append(np.copy(h_ex));
-            tblocks.append(-tl*np.eye(*np.shape(h_ex)) );
-        hblocks.append(np.zeros_like(h_ex) );
-        hblocks = np.array(hblocks);
-        tblocks = np.array(tblocks);
-        if (N==2): print(hblocks, "\n", tblocks);
+# entangle the me up states into eric's me, s12, m12> = up, 2, 1> state
+h_SR = wfm.utils.entangle(h_SR, *pair);
+if(verbose > 4):
+    h_SR_32 = fci_mod.single_to_det(h1e,g2e, species, states, dets_interest=dets32);
+    h_SR_32 = wfm.utils.entangle(h_SR_32, 0, 1);
+    print("\n- Entangled hamiltonian\n", h_SR_32);
 
-        # coefs
-        Tvals.append(wfm.Tcoef(hblocks, tblocks, tl, Energy, source));
+# iter over N
+Nmax = 80
+Nvals = np.linspace(1,Nmax,min(Nmax,20),dtype = int);
+Tvals = [];
+for N in Nvals:
 
-    # plot
-    Tvals = np.array(Tvals);
-    fig, ax = plt.subplots();
-    if False:
-        ax.scatter(Nvals, Tvals[:,0], marker = 's', label = '|1/2, 1/2, 2, 1>');
-        ax.scatter(Nvals, Tvals[:,1], marker = 's', label = '|1/2, 1/2, 1, 1>');
-        ax.scatter(Nvals, Tvals[:,2], marker = 's', label = '|1/2,-1/2, 2, 2>');
-    else:
-        ax.plot(Nvals, Tvals[:,0], label = '|1/2, 1/2> |2, 1>');
-        ax.plot(Nvals, Tvals[:,1], label = '|1/2, 1/2> |1, 1>');
-        ax.plot(Nvals, Tvals[:,2], label = '|1/2,-1/2> |2, 2>');
+    # package as block hams 
+    # number of blocks depends on N
+    hblocks = [np.zeros_like(h_SR)]
+    tblocks = [-tl*np.eye(*np.shape(h_SR)) ];
+    for Ni in range(N):
+        hblocks.append(np.copy(h_SR));
+        tblocks.append(-tl*np.eye(*np.shape(h_SR)) );
+    hblocks.append(np.zeros_like(h_SR) );
+    hblocks = np.array(hblocks);
+    tblocks = np.array(tblocks);
 
-    # format
-    #ax.set_title("Transmission at resonance, $J_K = 2D/3$");
-    ax.set_ylabel("$T$");
-    ax.set_xlabel("$N$");
-    ax.set_ylim(0.0,1.05);
-    plt.legend();
-    ax.minorticks_on();
-    ax.grid(which='major', color='#DDDDDD', linewidth=0.8);
-    ax.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5);
-    plt.show();
+    # coefs
+    Tvals.append(wfm.kernel(hblocks, tblocks, tl, Energy, source));
 
+# plot
+Tvals = np.array(Tvals);
+fig, ax = plt.subplots();
+ax.scatter(Nvals, Tvals[:,sourcei], marker = 's', label = "$|d>$");
+ax.scatter(Nvals, Tvals[:,pair[0]], marker = 's', label = "$|+>$");
+ax.scatter(Nvals, Tvals[:,pair[1]], marker = 's', label = "$|->$");
 
-elif option == "2q": # second quantized form of eric's model
-
-    # define source
-    source = np.zeros(3);
-    source[2] = 1;  # me, m1, m2 = down, 1, 1 state
-
-    # fix energy near bottom of band
-    Energy = -2*tl + 0.5;
-    ka = np.arccos(Energy/(-2*tl));
-
-    # iter over N
-    Nmax = 80
-    Nvals = np.linspace(1,Nmax,min(Nmax,20),dtype = int);
-    Tvals = [];
-    for N in Nvals:
-
-        # 2nd qu'd ham
-        h1e, g2e = ops.h_switzer(D, JH, JK, JK);
-
-        # check
-        #t1 = [(2,3),(3,2)];
-        #c1 = [1/np.sqrt(2),1/np.sqrt(2)];
-        #t1 = [(1,0),(0,1)];
-        #c1 = [1/np.sqrt(2),1/np.sqrt(2)];
-
-        # convert to many body form
-        parts = np.array([1,1,1]); # one particle each
-        states = [[0,1],[2,3,4],[5,6,7]]; # e up, down, spin 1 mz, spin 2 mz
-        interests = [[0,2,6],[0,3,5],[1,2,5]]; # pick me, m1, m2 = up, 1, 0>, up, 0, 1>, down, 1, 1 states
-        h_SR = fci_mod.single_to_det(h1e,g2e, parts, states, dets_interest = interests);
-        #print("D = ",D,", JH = ",JH,", JK1 = ", JK, ", JK2 = ",JK," pred = ", 2*D+JH-0.5*JK-0.5*JK);
-        #print(h_SR);
-
-        # entangle the me up states into eric's me, s12, m12> = up, 2, 1> state
-        h_SR = wfm.utils.entangle(h_SR, 0, 1);
-
-        # package as block hams 
-        # number of blocks depends on N
-        hblocks = [np.zeros_like(h_SR)]
-        tblocks = [-tl*np.eye(*np.shape(h_SR)) ];
-        for Ni in range(N):
-            hblocks.append(np.copy(h_SR));
-            tblocks.append(-tl*np.eye(*np.shape(h_SR)) );
-        hblocks.append(np.zeros_like(h_SR) );
-        hblocks = np.array(hblocks);
-        tblocks = np.array(tblocks);
-        if (N==2): print(hblocks, "\n", tblocks);
-
-        # coefs
-        Tvals.append(wfm.Tcoef(hblocks, tblocks, tl, Energy, source));
-
-    # plot
-    Tvals = np.array(Tvals);
-    fig, ax = plt.subplots();
-    ax.plot(Nvals, Tvals[:,0], label = '|1/2, 1/2> |2, 1>');
-    ax.plot(Nvals, Tvals[:,1], label = '|1/2, 1/2> |1, 1>');
-    ax.plot(Nvals, Tvals[:,2], label = '|1/2,-1/2> |2, 2>');
-
-    # format
-    #ax.set_title("Transmission at resonance, $J_K = 2D/3$");
-    ax.set_ylabel("$T$");
-    ax.set_xlabel("$N$");
-    ax.set_ylim(0.0,1.05);
-    plt.legend();
-    ax.minorticks_on();
-    ax.grid(which='major', color='#DDDDDD', linewidth=0.8);
-    ax.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5);
-    plt.show();
-        
-
-
-elif option == "JK": # iter over JK at fixed energy, then over N, looking for resonance
-
-    # eff params at resonance
-    # JK tbd
-    DeltaK = 0;
-    
-    # modulate
-    JKvals = np.linspace(-0.1,0.01,12);
-    maxvals = [];
-    for JK in JKvals:
-
-        print(">>>> JK = ",JK);
-
-        # iter over N, just looking for max 
-        Nmax = 180;
-        Nvals = np.linspace(1,Nmax,Nmax,dtype = int);
-        Nvals = Nvals[Nvals % 2 == 0 ];
-        Tvals = [];
-        for N in Nvals:
-
-            # eff exchange in SR
-            h_ex = (1/4)*np.array([[0,DeltaK,4*JK,              0,0,0,0,0],   # block diag m=3/2
-                             [DeltaK,-8*JH,-2*DeltaK,     0,0,0,0,0],
-                             [4*JK,-2*DeltaK,-6*JK+4*D,   0,0,0,0,0],
-                             [0,0,0,         0,0,0,0,0],   # block diag m=1/2
-                             [0,0,0,         0,0,0,0,0],
-                             [0,0,0,         0,0,0,0,0],
-                             [0,0,0,         0,0,0,0,0],
-                             [0,0,0,         0,0,0,0,0]]);
-
-            # package as block hams 
-            # number of blocks depends on N
-            hblocks = [np.zeros_like(h_ex)]
-            tblocks = [-tl*np.eye(*np.shape(h_ex)) ];
-            for Ni in range(N):
-                hblocks.append(np.copy(h_ex));
-                tblocks.append(-tl*np.eye(*np.shape(h_ex)) );
-            hblocks.append(np.zeros_like(h_ex) );
-            hblocks = np.array(hblocks);
-            tblocks = np.array(tblocks);
-
-            # get transmission coefs
-            Energy = -2*tl + 0.5;
-            Tvals.append(wfm.Tcoef(hblocks, tblocks, tl, Energy, source));
-
-        # get max of |up, 2, 1>
-        Tvals = np.array(Tvals);
-        maxvals.append(np.max(Tvals[:,0]));
-
-        # plot vs N at this JK
-        Tvals = np.array(Tvals);
-        fig, ax = plt.subplots();
-        ax.scatter(Nvals, Tvals[:,0], marker = 's', label = '|1/2, 1/2> |2, 1>');
-        ax.scatter(Nvals, Tvals[:,1], marker = 's', label = '|1/2, 1/2> |1, 1>');
-        ax.scatter(Nvals, Tvals[:,2], marker = 's', label = '|1/2,-1/2> |2, 2>');
-
-        # format
-        #ax.set_title("");
-        ax.set_ylabel("$T$");
-        ax.set_xlabel("$N$");
-        ax.set_ylim(0.0,1.05);
-        plt.legend();
-        ax.minorticks_on();
-        ax.grid(which='major', color='#DDDDDD', linewidth=0.8);
-        ax.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5);
-        plt.show();
-
-    # plot max vals vs JK
-    fig, ax = plt.subplots()
-    ax.scatter(JKvals, maxvals, marker = 's');
-    ax.axvline(D*2/3, color = "black", linestyle = "dashed");
-    ax.set_ylabel("max($T_{flip}$)");
-    ax.set_xlabel("$J_K$");
-    ax.set_ylim(0.0,1.05);
-    ax.minorticks_on();
-    ax.grid(which='major', color='#DDDDDD', linewidth=0.8);
-    ax.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5);
-    plt.show();
-    
-
-    
-
-
+# format
+#ax.set_title("Transmission at resonance, $J_K = 2D/3$");
+ax.set_ylabel("$T$");
+ax.set_xlabel("$N$");
+ax.set_ylim(0.0,1.05);
+plt.legend();
+ax.minorticks_on();
+ax.grid(which='major', color='#DDDDDD', linewidth=0.8);
+ax.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5);
+plt.show();
 
 
 
