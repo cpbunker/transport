88af624f7d2193835f11045b81ea9ca7d34ea3e1
Christian Bunker <christian@bunkerplanet.com>
2023-01-03 15:10:42 -0700

    Code in run_barrier_bardeen for plots that clearly show benefits of increased well size and larger well cover up in HL and HR for reducing error

--- run_barrier_bardeen.py
+++ run_barrier_bardeen.py
@@ -32,11 +32,11 @@ n_loc_dof = 1;
 tL = 1.0*np.eye(n_loc_dof);
 tinfty = 1.0*tL;
 tR = 1.0*tL;
-ts = (tinfty, tL, tinfty, tR, tinfty);
+# ts = (tinfty, tL, tinfty, tR, tinfty);
 Vinfty = 0.5*tL;
 VL = 0.0*tL;
 VR = 0.0*tL;
-Vs = (Vinfty, VL, Vinfty, VR, Vinfty);
+# Vs = (Vinfty, VL, Vinfty, VR, Vinfty);
 
 def get_ideal_T(Es,mytL,myVL,mytC,myVC,myNC):
     '''
@@ -65,9 +65,9 @@ def print_H_j(H):
 #### benchmarking T in spinless 1D case
 
 # T vs NL
-if True:
+if False:
 
-    NLvals = [50,100];
+    NLvals = [50,100,500];
     numplots = len(NLvals);
     fig, axes = plt.subplots(numplots, sharex = True);
     if numplots == 1: axes = [axes];
@@ -86,14 +86,7 @@ if True:
     print_H_j(HC);
 
     # central region prime
-    tCprime = tC;
-    VCprime = VC;
-    HCprime = np.zeros((NC,NC,n_loc_dof,n_loc_dof));
-    for j in range(NC):
-        HCprime[j,j] = VCprime;
-    for j in range(NC-1):
-        HCprime[j,j+1] = -tCprime;
-        HCprime[j+1,j] = -tCprime;
+    HCprime = np.copy(HC);
     print_H_j(HCprime);
 
     # bardeen results for different well widths
@@ -101,13 +94,18 @@ if True:
         Ninfty = 50;
         NL = NLvals[NLi];
         NR = 1*NL;
-        Evals, Tvals = bardeen.kernel(*ts, *Vs, Ninfty, NL, NR, HC, HCprime,cutoff=VC[0,0],verbose=verbose);
+        # bardeen.kernel syntax:
+        # tinfty, tL, tLprime, tR, tRprime,
+        # Vinfty, VL, VLprime, VR, VRprime,
+        # Ninfty, NL, NR, HC,HCprime,
+        Evals, Tvals = bardeen.kernel(tinfty, tL, tinfty, tR, tinfty,
+                                      Vinfty, VL, Vinfty, VR, Vinfty,
+                                      Ninfty, NL, NR, HC, HCprime,cutoff=VC[0,0],verbose=verbose);
 
         # % error
         axright = axes[NLi].twinx();
-        axright.set_ylabel("$\%$ error",fontsize=myfontsize);
-        axright.set_ylim(0,20);
-        
+
+        # for each dof
         for alpha in range(n_loc_dof): 
 
             # truncate to bound states and plot
@@ -121,8 +119,186 @@ if True:
             #axes[NLi].set_ylim(0,1.1*max(Tvals[alpha]));
             axright.plot(xvals,100*abs((yvals-np.real(ideal_Tvals_alpha))/ideal_Tvals_alpha),color=accentcolors[1]);
 
+        # format
+        axright.set_ylabel("$\%$ error",fontsize=myfontsize);
+        axright.set_ylim(0,50);
         axes[NLi].set_ylabel('$T$',fontsize=myfontsize);
         axes[NLi].set_title('$N_L = '+str(NLvals[NLi])+'$', x=0.2, y = 0.7, fontsize=myfontsize);
+
+    # format and show
+    axes[-1].set_xscale('log', subs = []);
+    axes[-1].set_xlabel('$(\\varepsilon_m + 2t_L)/t_L$',fontsize=myfontsize);
+    plt.tight_layout();
+    plt.show();
+
+# T vs VLR prime
+if False:
+
+    Vprimevals = [Vinfty/10,Vinfty/5,Vinfty];
+    numplots = len(Vprimevals);
+    fig, axes = plt.subplots(numplots, sharex = True);
+    if numplots == 1: axes = [axes];
+    fig.set_size_inches(7/2,3*numplots/2);
+
+    # central region
+    tC = 1*tL;
+    VC = 0.1*tL;
+    NC = 11;
+    HC = np.zeros((NC,NC,n_loc_dof,n_loc_dof));
+    for j in range(NC):
+        HC[j,j] = VC;
+    for j in range(NC-1):
+        HC[j,j+1] = -tC;
+        HC[j+1,j] = -tC;
+    print_H_j(HC);
+
+    # central region prime
+    HCprime = np.copy(HC);
+    print_H_j(HCprime);
+
+    # bardeen results for heights of barrier covering well
+    for Vprimei in range(len(Vprimevals)):
+        Ninfty = 50;
+        NL = 100;
+        NR = 1*NL;
+        VLprime = Vprimevals[Vprimei];
+        VRprime = Vprimevals[Vprimei];
+        # bardeen.kernel syntax:
+        # tinfty, tL, tLprime, tR, tRprime,
+        # Vinfty, VL, VLprime, VR, VRprime,
+        # Ninfty, NL, NR, HC,HCprime,
+        Evals, Tvals = bardeen.kernel(tinfty, tL, tinfty, tR, tinfty,
+                                      Vinfty, VL, VLprime, VR, VRprime,
+                                      Ninfty, NL, NR, HC, HCprime,cutoff=VC[0,0],verbose=verbose);
+
+        # % error
+        axright = axes[Vprimei].twinx();
+
+        # for each dof
+        for alpha in range(n_loc_dof): 
+
+            # truncate to bound states and plot
+            yvals = np.diagonal(Tvals[alpha,:,alpha,:]);
+            xvals = np.real(Evals[alpha])+2*tL[alpha,alpha];
+            axes[Vprimei].scatter(xvals, yvals, marker=mymarkers[0], color=mycolors[0]);
+
+            # compare
+            ideal_Tvals_alpha = get_ideal_T(xvals,tL[alpha,alpha],VL[alpha,alpha],tC[alpha,alpha],VC[alpha,alpha],NC);
+            axes[Vprimei].plot(xvals,np.real(ideal_Tvals_alpha), color=accentcolors[0], linewidth=mylinewidth);
+            #axes[Vprimei].set_ylim(0,1.1*max(Tvals[alpha]));
+            axright.plot(xvals,100*abs((yvals-np.real(ideal_Tvals_alpha))/ideal_Tvals_alpha),color=accentcolors[1]);
+
+        # format
+        axright.set_ylabel("$\%$ error",fontsize=myfontsize);
+        axright.set_ylim(0,50);
+        axes[Vprimei].set_ylabel('$T$',fontsize=myfontsize);
+        axes[Vprimei].set_title("$V_L' = "+str(Vprimevals[Vprimei])+'$', x=0.2, y = 0.7, fontsize=myfontsize);
+
+    # format and show
+    axes[-1].set_xscale('log', subs = []);
+    axes[-1].set_xlabel('$(\\varepsilon_m + 2t_L)/t_L$',fontsize=myfontsize);
+    plt.tight_layout();
+    plt.show();
+
+# worst case vs best case
+if True:
+
+    numplots = 2;
+    fig, axes = plt.subplots(numplots, sharex = True);
+    if numplots == 1: axes = [axes];
+    fig.set_size_inches(7/2,3*numplots/2);
+
+    # central region
+    tC = 1*tL;
+    VC = 0.1*tL;
+    NC = 11;
+    HC = np.zeros((NC,NC,n_loc_dof,n_loc_dof));
+    for j in range(NC):
+        HC[j,j] = VC;
+    for j in range(NC-1):
+        HC[j,j+1] = -tC;
+        HC[j+1,j] = -tC;
+    print_H_j(HC);
+
+    # central region prime
+    HCprime = np.copy(HC);
+    print_H_j(HCprime);
+
+    # bardeen results for worst case
+    axno = 0;
+    Ninfty = 50;
+    NL = 50;
+    NR = 1*NL;
+    VLprime = Vinfty/10;
+    VRprime = Vinfty/10;
+    # bardeen.kernel syntax:
+    # tinfty, tL, tLprime, tR, tRprime,
+    # Vinfty, VL, VLprime, VR, VRprime,
+    # Ninfty, NL, NR, HC,HCprime,
+    Evals, Tvals = bardeen.kernel(tinfty, tL, tinfty, tR, tinfty,
+                                  Vinfty, VL, VLprime, VR, VRprime,
+                                  Ninfty, NL, NR, HC, HCprime,cutoff=VC[0,0],verbose=verbose);
+
+    # % error
+    axright = axes[axno].twinx();
+
+    # for each dof
+    for alpha in range(n_loc_dof): 
+
+        # truncate to bound states and plot
+        yvals = np.diagonal(Tvals[alpha,:,alpha,:]);
+        xvals = np.real(Evals[alpha])+2*tL[alpha,alpha];
+        axes[axno].scatter(xvals, yvals, marker=mymarkers[0], color=mycolors[0]);
+
+        # compare
+        ideal_Tvals_alpha = get_ideal_T(xvals,tL[alpha,alpha],VL[alpha,alpha],tC[alpha,alpha],VC[alpha,alpha],NC);
+        axes[axno].plot(xvals,np.real(ideal_Tvals_alpha), color=accentcolors[0], linewidth=mylinewidth);
+        #axes[axno].set_ylim(0,1.1*max(Tvals[alpha]));
+        axright.plot(xvals,100*abs((yvals-np.real(ideal_Tvals_alpha))/ideal_Tvals_alpha),color=accentcolors[1]);
+
+    # format
+    axright.set_ylabel("$\%$ error",fontsize=myfontsize);
+    axright.set_ylim(0,50);
+    axes[axno].set_ylabel('$T$',fontsize=myfontsize);
+    axes[axno].set_title("Worst", x=0.2, y = 0.7, fontsize=myfontsize);
+
+    # bardeen results for best case
+    axno = 1;
+    Ninfty = 50;
+    NL = 500;
+    NR = 1*NL;
+    VLprime = Vinfty;
+    VRprime = Vinfty;
+    # bardeen.kernel syntax:
+    # tinfty, tL, tLprime, tR, tRprime,
+    # Vinfty, VL, VLprime, VR, VRprime,
+    # Ninfty, NL, NR, HC,HCprime,
+    Evals, Tvals = bardeen.kernel(tinfty, tL, tinfty, tR, tinfty,
+                                  Vinfty, VL, VLprime, VR, VRprime,
+                                  Ninfty, NL, NR, HC, HCprime,cutoff=VC[0,0],verbose=verbose);
+
+    # % error
+    axright = axes[axno].twinx();
+
+    # for each dof
+    for alpha in range(n_loc_dof): 
+
+        # truncate to bound states and plot
+        yvals = np.diagonal(Tvals[alpha,:,alpha,:]);
+        xvals = np.real(Evals[alpha])+2*tL[alpha,alpha];
+        axes[axno].scatter(xvals, yvals, marker=mymarkers[0], color=mycolors[0]);
+
+        # compare
+        ideal_Tvals_alpha = get_ideal_T(xvals,tL[alpha,alpha],VL[alpha,alpha],tC[alpha,alpha],VC[alpha,alpha],NC);
+        axes[axno].plot(xvals,np.real(ideal_Tvals_alpha), color=accentcolors[0], linewidth=mylinewidth);
+        #axes[axno].set_ylim(0,1.1*max(Tvals[alpha]));
+        axright.plot(xvals,100*abs((yvals-np.real(ideal_Tvals_alpha))/ideal_Tvals_alpha),color=accentcolors[1]);
+
+    # format
+    axright.set_ylabel("$\%$ error",fontsize=myfontsize);
+    axright.set_ylim(0,50);
+    axes[axno].set_ylabel('$T$',fontsize=myfontsize);
+    axes[axno].set_title("Best", x=0.2, y = 0.7, fontsize=myfontsize);
 
     # format and show
     axes[-1].set_xscale('log', subs = []);
