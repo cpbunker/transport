b3f868eb7c2e5b860c2be0c7cd13f7ff437983bc
Christian Bunker <christian@bunkerplanet.com>
2023-02-25 21:57:36 -0500

    rough draft of SWAP gate

--- run_wfm_swap.py
+++ run_wfm_swap.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 verbose = 1;
 
 # fig standardizing
-myxvals = 49;
+myxvals = 99;
 myfontsize = 14;
 mycolors = ["cornflowerblue", "darkgreen", "darkred", "darkcyan", "darkmagenta","darkgray"];
 accentcolors = ["black","red"];
@@ -29,15 +29,50 @@ mypanels = ["(a)","(b)","(c)","(d)"];
 #plt.rcParams.update({"text.usetex": True,"font.family": "Times"})
 
 # constructing the hamiltonian
-def reduced_ham(params, S) -> np.ndarray:
-    D1, D2, J12, JK1, JK2 = params;
-    assert(D1 == D2);
-    h = np.array([[S*S*D1+(S-1)*(S-1)*D2+S*(S-1)*J12+(JK1/2)*S+(JK2/2)*(S-1), S*J12, np.sqrt(2*S)*(JK2/2) ], # up, s, s-1
-                    [S*J12, (S-1)*(S-1)*D1+S*S*D2+S*(S-1)*J12+(JK1/2)*S + (JK2/2)*(S-1), np.sqrt(2*S)*(JK1/2) ], # up, s-1, s
-                    [np.sqrt(2*S)*(JK2/2), np.sqrt(2*S)*(JK1/2),S*S*D1+S*S*D2+S*S*J12+(-JK1/2)*S +(-JK2/2)*S]], # down, s, s
-                   dtype = complex);
+def h_cicc(J, i1, i2) -> np.ndarray: 
+    '''
+    TB matrices for ciccarrello system (1 electron, 2 spin-1/2s)
+    Args:
+    - J, float, eff heisenberg coupling
+    - i1, list of sites for first spin-1/2
+    - i2, list of sites for second spin-1/2
+    '''
+    if(not isinstance(i1, list) or not isinstance(i2, list)): raise TypeError;
+    assert(i1[0] == 1);
+    if(not i1[-1] < i2[0]): raise Exception("i1 and i2 cannot overlap");
+    NC = i2[-1]; # num sites in the central region
+    
+    # heisenberg interaction matrices
+    Se_dot_S1 = (J/4.0)*np.array([ [1,0,0,0,0,0,0,0], # coupling to first spin impurity
+                        [0,1,0,0,0,0,0,0],
+                        [0,0,-1,0,2,0,0,0],
+                        [0,0,0,-1,0,2,0,0],
+                        [0,0,2,0,-1,0,0,0],
+                        [0,0,0,2,0,-1,0,0],
+                        [0,0,0,0,0,0,1,0],
+                        [0,0,0,0,0,0,0,1] ]);
+    Se_dot_S2 = (J/4.0)*np.array([ [1,0,0,0,0,0,0,0], # coupling to second spin impurity
+                        [0,-1,0,0,2,0,0,0],
+                        [0,0,1,0,0,0,0,0],
+                        [0,0,0,-1,0,0,2,0],
+                        [0,2,0,0,-1,0,0,0],
+                        [0,0,0,0,0,1,0,0],
+                        [0,0,0,2,0,0,-1,0],
+                        [0,0,0,0,0,0,0,1] ]);
 
-    return h;
+    # insert these local interactions
+    h_cicc =[];
+    Nsites = NC+1; # N sites in SR + 1 for LL
+    for sitei in range(Nsites): # iter over all sites
+        if(sitei in i1 and sitei not in i2):
+            h_cicc.append(Se_dot_S1);
+        elif(sitei in i2 and sitei not in i1):
+            h_cicc.append(Se_dot_S2);
+        elif(sitei not in i1 and sitei not in i2):
+            h_cicc.append(np.zeros_like(Se_dot_S1) );
+        else:
+            raise Exception("i1 and i2 cannot overlap");
+    return np.array(h_cicc, dtype=complex);
             
 #########################################################
 #### barrier in right lead for total reflection
@@ -46,27 +81,20 @@ if False: # distance of the barrier NB on the x axis
 
     # tight binding params
     tl = 1.0;
-    tp = 1.0;
-    JK = -0.5*tl/100;
-    J12 = 0*tl/100;
-    Dval = 1.0/100; # order of D: 0.1 meV for Mn to 1 meV for MnPc
     myspinS = 0.5;
-    n_loc_dof = 3;
-
-    if True:
-        myspinS = 0.5;
-        Dval=0.0;
-        JK=-0.1;
+    n_loc_dof = int((2*myspinS+1)**3);
+    n_mol_dof = int((2*myspinS+1)**2);
+    Jval = -0.1;
 
     # axes
-    ylabels = ["\\uparrow \\uparrow \downarrow","\\uparrow \downarrow \\uparrow","\downarrow \\uparrow \\uparrow"];
-    nrows, ncols = n_loc_dof, n_loc_dof;
+    ylabels = ["\downarrow \\uparrow \\uparrow","\downarrow \\uparrow \downarrow","\downarrow \downarrow \\uparrow","\downarrow \downarrow \downarrow"];
+    nrows, ncols = n_mol_dof, n_mol_dof;
     fig, axes = plt.subplots(nrows, ncols, sharex=True);
     fig.set_size_inches(ncols*7/2,nrows*3/2);
 
     # iter over E (colors)
-    rhoJvals = np.array([0.5,1.0,2.0]);
-    Evals = JK*JK/(rhoJvals*rhoJvals*np.pi*np.pi*tl);
+    rhoJvals = np.array([0.5,1.0,5.0]);
+    Evals = Jval*Jval/(rhoJvals*rhoJvals*np.pi*np.pi*tl);
     Rvals = np.empty((n_loc_dof,n_loc_dof,len(Evals),myxvals)); # by  init spin, final spin, energy, NB
     for Evali in range(len(Evals)):
 
@@ -76,41 +104,26 @@ if False: # distance of the barrier NB on the x axis
         k_rho = np.arccos(Energy/(-2*tl)); # k corresponding to fixed \rho J a
         
         # iter over barrier distance (x axis)
-        kNBmax = 3*np.pi/4;
+        kNBmax = 0.5*np.pi;
         NBmax = int(kNBmax/k_rho);
         if(verbose): print(NBmax); #assert False;
-        NBvals = np.linspace(0,NBmax,myxvals,dtype=int);
+        NBvals = np.linspace(1,NBmax,myxvals,dtype=int);
         kNBvals = k_rho*NBvals;
         for NBvali in range(len(NBvals)):
             NBval = NBvals[NBvali];
 
             # construct hblocks from spin ham
-            hblocks = [];
-            impis = [1,2];
-            for j in range(4): # LL, imp 1, imp 2, RL
-                # define all physical params
-                JK1, JK2 = 0, 0;
-                if(j == impis[0]): JK1 = JK;
-                elif(j == impis[1]): JK2 = JK;
-                params = Dval, Dval, J12, JK1, JK2;
-                # construct h_SR (determinant basis)
-                hSR = reduced_ham(params,myspinS);
-                # don't entangle since SWAP is performed in logical basis
-                hblocks.append(np.copy(hSR));
-                if(verbose > 3 ):
-                    print("\nJK1, JK2 = ",JK1, JK2);
-                    print(" - ham:\n", hSR);
+            hblocks_cicc = h_cicc(Jval, [1],[2]);
 
             # add large barrier at end
-            tnn = [-tl*np.eye(n_loc_dof),-tp*np.eye(n_loc_dof),-tl*np.eye(n_loc_dof)];
-            for _ in range(NBval):
-                hblocks.append(np.zeros_like(hblocks[0]));
+            NC = len(hblocks_cicc); assert(NC==3); # num sites in central region
+            hblocks, tnn = [], []; # new empty array all the way to barrier, will add cicc later
+            for _ in range(NC+NBval):
+                hblocks.append(0.0*np.eye(n_loc_dof));
                 tnn.append(-tl*np.eye(n_loc_dof));
+            hblocks, tnn = np.array(hblocks,dtype=complex), np.array(tnn[:-1]);
+            hblocks[0:NC] += hblocks_cicc;
             hblocks[-1] += 5*tl*np.eye(n_loc_dof);
-
-                        
-            # hopping
-            tnn = np.array(tnn);
             tnnn = np.zeros_like(tnn)[:-1]; # no next nearest neighbor hopping
 
             # iter over sources
@@ -123,7 +136,7 @@ if False: # distance of the barrier NB on the x axis
                 E_shift = hblocks[0,sourcei,sourcei]; # const shift st hLL[sourcei,sourcei] = 0
                 for hb in hblocks:
                     hb += -E_shift*np.eye(n_loc_dof);
-               
+                    
                 # get R, T coefs
                 Rdum, Tdum = wfm.kernel(hblocks, tnn, tnnn, tl, Energy , source, all_debug = False);
                 Rvals[sourcei,:,Evali,NBvali] = Rdum;
@@ -131,47 +144,45 @@ if False: # distance of the barrier NB on the x axis
         #### end loop over NB
 
         # determine kNB*
-        kNBstar = kNBvals[np.argmin(Rvals[0,0,Evali])];
+        kNBstar = kNBvals[np.argmin(Rvals[1,1,Evali])];
          
         # plot R_\sigma vs NBvals
-        for sourcei in range(n_loc_dof):
-            for sigmai in range(n_loc_dof):
-                axes[sourcei,sigmai].plot(kNBvals/np.pi, Rvals[sourcei,sigmai,Evali], color=mycolors[Evali], marker=mymarkers[1+Evali], markevery=mymarkevery, linewidth=mylinewidth);
-                axes[sourcei,sigmai].axvline(kNBstar/np.pi, color=mycolors[Evali]);
-                axes[sourcei,sigmai].set_title("$"+str(ylabels[sourcei])+"\\rightarrow"+str(ylabels[sigmai])+"$");
-                axes[sourcei,sigmai].set_ylim(0.0,1.0);
+        elecspin = 1;
+        for sourcei in range(n_mol_dof):
+            for sigmai in range(n_mol_dof):
+                axes[sourcei,sigmai].plot(kNBvals/np.pi, Rvals[4*elecspin+sourcei,4*elecspin+sigmai,Evali], color=mycolors[Evali], marker=mymarkers[1+Evali], markevery=mymarkevery, linewidth=mylinewidth);
+                if(sourcei == sigmai): axes[sourcei,sigmai].axvline(kNBstar/np.pi, color=mycolors[Evali]);
+                axes[sourcei,sigmai].set_title("$R("+str(ylabels[sourcei])+"\\rightarrow"+str(ylabels[sigmai])+")$");
+                axes[sourcei,sigmai].set_ylim(-0.1,1.1);
+                axes[sourcei,sigmai].axhline(0.0,color='lightgray',linestyle='dashed');
+                axes[sourcei,sigmai].axhline(1.0,color='lightgray',linestyle='dashed');
+                axes[-1,sigmai].set_xlim(0,0.5);
                 axes[-1,sigmai].set_xlabel('$k_i aN_B /\pi$',fontsize=myfontsize);
+                #axes[sourcei,-1].plot(kNBvals/np.pi, np.sum(Rvals[4*elecspin+sourcei,:,Evali],axis=0), color='red');
                     
     # show
     plt.tight_layout();
     plt.show();
 
     # save data
-    param_vals = np.array([tl,tp,JK,J12,Dval,myspinS,n_loc_dof]);
+    param_vals = np.array([myspinS,tl,Jval]);
     fname = "data/wfm_swap/NB/"+str(rhoJvals.round(1));
-    np.savetxt(fname+".txt", Evals, header="[tl, tp, JK, J12, Dval, myspinS, n_loc_dof] =\n"+str(param_vals)+"\nEvals =");
-    np.save(fname+"_x", kNBvals/np.pi);
-    np.save(fname, Rvals);
+    #np.savetxt(fname+".txt", Evals, header="[tl, tp, JK, J12, Dval, myspinS, n_loc_dof] =\n"+str(param_vals)+"\nEvals =");
+    #np.save(fname+"_x", kNBvals/np.pi);
+    #np.save(fname, Rvals);
 
 if True: # incident kinetic energy on the x axis
 
     # tight binding params
     tl = 1.0;
-    tp = 1.0;
-    JK = -0.5*tl/100;
-    J12 = 0*tl/100;
-    Dval = 1.0/100; # order of D: 0.1 meV for Mn to 1 meV for MnPc
-    myspinS = 1.0;
-    n_loc_dof = 3;
-
-    if True:
-        myspinS = 0.5;
-        Dval=0;
-        JK=-0.1;
+    myspinS = 0.5;
+    n_loc_dof = int((2*myspinS+1)**3);
+    n_mol_dof = int((2*myspinS+1)**2);
+    Jval = -0.1;
 
     # axes
-    ylabels = ["\\uparrow \\uparrow \downarrow","\\uparrow \downarrow \\uparrow","\downarrow \\uparrow \\uparrow"];
-    nrows, ncols = n_loc_dof, n_loc_dof;
+    ylabels = ["\downarrow \\uparrow \\uparrow","\downarrow \\uparrow \downarrow","\downarrow \downarrow \\uparrow","\downarrow \downarrow \downarrow"];
+    nrows, ncols = n_mol_dof, n_mol_dof;
     fig, axes = plt.subplots(nrows, ncols, sharex=True);
     fig.set_size_inches(ncols*7/2,nrows*3/2);
 
@@ -181,8 +192,8 @@ if True: # incident kinetic energy on the x axis
     for NBvali in range(len(kNBvals)):
 
         # iter over E 
-        rhoJvals = np.linspace(0.5,2.0,myxvals);
-        Evals = JK*JK/(rhoJvals*rhoJvals*np.pi*np.pi*tl);
+        rhoJvals = np.linspace(0.5,5.0,myxvals);
+        Evals = Jval*Jval/(rhoJvals*rhoJvals*np.pi*np.pi*tl);
         for Evali in range(len(Evals)):
 
             # energy
@@ -195,31 +206,17 @@ if True: # incident kinetic energy on the x axis
             if(verbose): print(NBval); #assert False;
 
             # construct hblocks from spin ham
-            hblocks = [];
-            impis = [1,2];
-            for j in range(4): # LL, imp 1, imp 2, RL
-                # define all physical params
-                JK1, JK2 = 0, 0;
-                if(j == impis[0]): JK1 = JK;
-                elif(j == impis[1]): JK2 = JK;
-                params = Dval, Dval, J12, JK1, JK2;
-                # construct h_SR (determinant basis)
-                hSR = reduced_ham(params,myspinS);
-                # don't entangle since SWAP is performed in logical basis
-                hblocks.append(np.copy(hSR));
-                if(verbose > 3 ):
-                    print("\nJK1, JK2 = ",JK1, JK2);
-                    print(" - ham:\n", hSR);
+            hblocks_cicc = h_cicc(Jval, [1],[2]);
 
             # add large barrier at end
-            tnn = [-tl*np.eye(n_loc_dof),-tp*np.eye(n_loc_dof),-tl*np.eye(n_loc_dof)];
-            for _ in range(NBval):
-                hblocks.append(np.zeros_like(hblocks[0]));
+            NC = len(hblocks_cicc); assert(NC==3); # num sites in central region
+            hblocks, tnn = [], []; # new empty array all the way to barrier, will add cicc later
+            for _ in range(NC+NBval):
+                hblocks.append(0.0*np.eye(n_loc_dof));
                 tnn.append(-tl*np.eye(n_loc_dof));
+            hblocks, tnn = np.array(hblocks,dtype=complex), np.array(tnn[:-1]);
+            hblocks[0:NC] += hblocks_cicc;
             hblocks[-1] += 5*tl*np.eye(n_loc_dof);
-
-            # hopping
-            tnn = np.array(tnn);
             tnnn = np.zeros_like(tnn)[:-1]; # no next nearest neighbor hopping
 
             # iter over sources
@@ -240,27 +237,32 @@ if True: # incident kinetic energy on the x axis
         #### end loop over E
 
         # determine rhoJa*
-        rhoJstar = rhoJvals[np.argmin(Rvals[0,0,:,NBvali])];
+        rhoJstar = rhoJvals[np.argmin(Rvals[1,1,:,NBvali])];
              
         # plot R_\sigma vs NBvals
-        for sourcei in range(n_loc_dof):
-            for sigmai in range(n_loc_dof):
-                axes[sourcei,sigmai].plot(rhoJvals, Rvals[sourcei,sigmai,:,NBvali], color=mycolors[NBvali], marker=mymarkers[1+NBvali], markevery=mymarkevery, linewidth=mylinewidth);
-                axes[sourcei,sigmai].axvline(rhoJstar, color=mycolors[NBvali]);
+        elecspin = 1;
+        for sourcei in range(n_mol_dof):
+            for sigmai in range(n_mol_dof):
+                axes[sourcei,sigmai].plot(rhoJvals, Rvals[4*elecspin+sourcei,4*elecspin+sigmai,:,NBvali], color=mycolors[NBvali], marker=mymarkers[1+NBvali], markevery=mymarkevery, linewidth=mylinewidth);
+                #if(sourcei == sigmai): axes[sourcei,sigmai].axvline(rhoJstar, color=mycolors[NBvali]);
                 axes[sourcei,sigmai].set_title("$R("+str(ylabels[sourcei])+"\\rightarrow"+str(ylabels[sigmai])+")$");
-                axes[sourcei,sigmai].set_ylim(0.0,1.0);
+                axes[sourcei,sigmai].set_ylim(-0.1,1.1);
+                axes[sourcei,sigmai].axhline(0.0,color='lightgray',linestyle='dashed');
+                axes[sourcei,sigmai].axhline(1.0,color='lightgray',linestyle='dashed');
+                axes[-1,sigmai].set_xlim(0.5,5.0);
                 axes[-1,sigmai].set_xlabel('$\\rho Ja$',fontsize=myfontsize);
+                #axes[sourcei,-1].plot(kNBvals/np.pi, np.sum(Rvals[4*elecspin+sourcei,4*elecspin+sigmai,:,NBvali],axis=0), color='red');
                     
     # show
     plt.tight_layout();
     plt.show();
 
     # save data
-    param_vals = np.array([tl,tp,JK,J12,Dval,myspinS,n_loc_dof]);
-    fname = "data/wfm_swap/E/"+str((kNBvals/np.pi).round(1));
-    np.savetxt(fname+".txt", kNBvals, header="[tl, tp, JK, J12, Dval, myspinS, n_loc_dof] =\n"+str(param_vals)+"\nkNBvals =");
-    np.save(fname+"_x", rhoJvals);
-    np.save(fname, Rvals);
+    param_vals = np.array([myspinS,tl,Jval]);
+    #fname = "data/wfm_swap/E/"+str((kNBvals/np.pi).round(1));
+    #np.savetxt(fname+".txt", kNBvals, header="[tl, tp, JK, J12, Dval, myspinS, n_loc_dof] =\n"+str(param_vals)+"\nkNBvals =");
+    #np.save(fname+"_x", rhoJvals);
+    #np.save(fname, Rvals);
 
 
 
