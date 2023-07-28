cc2a00f679b88eec897508b156b78cda6af49053
Christian Bunker <christian@bunkerplanet.com>
2023-07-28 11:50:09 -0400

    Mn fits

--- transport/simmons/oscillations.py
+++ transport/simmons/oscillations.py
@@ -575,7 +575,7 @@ def plot_saved_fit(stop_at, metal, combined=[], verbose = 1):
 
 if(__name__ == "__main__"):
 
-    metal = "Mnv2/"; # tells which experimental data to load
+    metal = "Mn/"; # tells which experimental data to load
     stop_ats = ['imp_mag/', 'sin/', 'imp/','mag/','lorentz_zero/', 'lorentz/'];
     stop_at = stop_ats[-1];
     verbose=1;
