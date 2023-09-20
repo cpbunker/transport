7cb863cbb5a89f22a672a76dadc4ebc2345d4313
Christian Bunker <christian@bunkerplanet.com>
2023-09-20 17:10:50 -0400

    transition from rhoJa to log(Ki) in run_wfm_swap

--- run_wfm_swap_notlog.py
+++ run_wfm_swap_notlog.py
@@ -93,15 +93,16 @@ if False: # distance of the barrier NB on the x axis
     fig, axes = plt.subplots(nrows, ncols, sharex='col', sharey = 'row');
     fig.set_size_inches(ncols*7/2,nrows*3/2);
 
-    # iter over incident kinetic energy (colors)
-    Kvals = np.array([0.0001,0.001]); # incident kinetic energy/t
-    Rvals = np.empty((n_loc_dof,n_loc_dof,len(Kvals),myxvals)); # by  init spin, final spin, energy, NB
-    for Kvali in range(len(Kvals)):
+    # iter over E (colors)
+    rhoJvals = np.array([0.5,1.0,5.0]);
+    Evals = Jval*Jval/(rhoJvals*rhoJvals*np.pi*np.pi*tl);
+    Rvals = np.empty((n_loc_dof,n_loc_dof,len(Evals),myxvals)); # by  init spin, final spin, energy, NB
+    for Evali in range(len(Evals)):
 
         # energy
-        Kval = Kvals[Kvali]; # Kval > 0 always, what I call K_i in paper
-        Energy = Kval - 2*tl; # -2t < Energy < 2t, what I call E in paper
-        k_rho = np.arccos(Energy/(-2*tl)); # k corresponding to fixed energy
+        Eval = Evals[Evali]; # Eval > 0 always, what I call K in paper
+        Energy = Eval - 2*tl; # -2t < Energy < 2t, what I call E in paper
+        k_rho = np.arccos(Energy/(-2*tl)); # k corresponding to fixed \rho J a
         
         # iter over barrier distance (x axis)
         kNBmax = 0.5*np.pi;
@@ -125,7 +126,7 @@ if False: # distance of the barrier NB on the x axis
             hblocks[0:NC] += hblocks_cicc;
             hblocks[-1] += VB*np.eye(n_loc_dof);
             tnnn = np.zeros_like(tnn)[:-1]; # no next nearest neighbor hopping
-            if(Kvali == 0 and NBvali == 0): print("\nhblocks = \n",np.real(hblocks));
+            if(Evali == 0 and NBvali == 0): print("\nhblocks = \n",np.real(hblocks));
 
             # iter over sources
             for sourcei in range(n_loc_dof):
@@ -140,19 +141,19 @@ if False: # distance of the barrier NB on the x axis
                     
                 # get R, T coefs
                 Rdum, Tdum = wfm.kernel(hblocks, tnn, tnnn, tl, Energy , source, all_debug = False);
-                Rvals[sourcei,:,Kvali,NBvali] = Rdum;
+                Rvals[sourcei,:,Evali,NBvali] = Rdum;
             #### end loop over sourcei
         #### end loop over NB
 
-        # determine kNB*, ie x val where the SWAP happens
-        kNBstar = kNBvals[np.argmin(Rvals[1,1,Kvali])];
+        # determine kNB*
+        kNBstar = kNBvals[np.argmin(Rvals[1,1,Evali])];
          
         # plot R_\sigma vs NBvals
-        elecspin = 1; # electron is spin down
+        elecspin = 1;
         for sourcei in range(n_mol_dof):
             for sigmai in range(n_mol_dof):
-                axes[sourcei,sigmai].plot(kNBvals/np.pi, Rvals[4*elecspin+sourcei,4*elecspin+sigmai,Kvali], color=mycolors[Kvali], marker=mymarkers[1+Kvali], markevery=mymarkevery, linewidth=mylinewidth);
-                #if(sourcei == sigmai): axes[sourcei,sigmai].axvline(kNBstar/np.pi, color=mycolors[EKali]);
+                axes[sourcei,sigmai].plot(kNBvals/np.pi, Rvals[4*elecspin+sourcei,4*elecspin+sigmai,Evali], color=mycolors[Evali], marker=mymarkers[1+Evali], markevery=mymarkevery, linewidth=mylinewidth);
+                #if(sourcei == sigmai): axes[sourcei,sigmai].axvline(kNBstar/np.pi, color=mycolors[Evali]);
                 axes[sourcei,sigmai].set_title("$R("+str(ylabels[sourcei])+"\\rightarrow"+str(ylabels[sigmai])+")$");
                 axes[sourcei,-1].set_ylim(-0.1,1.1);
                 axes[sourcei,-1].set_yticks([0,1.0]);
@@ -160,7 +161,7 @@ if False: # distance of the barrier NB on the x axis
                 axes[sourcei,sigmai].axhline(1.0,color='lightgray',linestyle='dashed');
                 axes[-1,sigmai].set_xlim(0,0.5);
                 axes[-1,sigmai].set_xlabel('$k_i aN_B /\pi$',fontsize=myfontsize);
-                #axes[sourcei,-1].plot(kNBvals/np.pi, np.sum(Rvals[4*elecspin+sourcei,:,Kvali],axis=0), color='red');
+                #axes[sourcei,-1].plot(kNBvals/np.pi, np.sum(Rvals[4*elecspin+sourcei,:,Evali],axis=0), color='red');
                     
     # show
     plt.tight_layout();
@@ -174,8 +175,6 @@ if False: # distance of the barrier NB on the x axis
     #np.save(fname, Rvals);
 
 if True: # incident kinetic energy on the x axis
-         # NB you STILL have to change NB
-         # this is where another voltage might be useful!!!
 
     # axes
     ylabels = ["\downarrow \\uparrow \\uparrow","\downarrow \\uparrow \downarrow","\downarrow \downarrow \\uparrow","\downarrow \downarrow \downarrow"];
@@ -183,20 +182,23 @@ if True: # incident kinetic energy on the x axis
     fig, axes = plt.subplots(nrows, ncols, sharex=True);
     fig.set_size_inches(ncols*7/2,nrows*3/2);
 
-    # iter over kNBvals (colors)
+    # iter over kNBvals
     kNBvals = np.pi*np.array([0.3]); # 0.4, 0.5
-    myxvals = 9
+    myxvals = 29
     Rvals = np.empty((n_loc_dof,n_loc_dof,myxvals,len(kNBvals))); # by  init spin, final spin, energy, NB
     for NBvali in range(len(kNBvals)):
 
-        # iter over incident kinetic energy (x axis)
-        Kvals = np.logspace(-4,-3,myxvals);
-        print("longest NB = ",int((kNBvals[-1]/np.arccos( (Kvals-2*tl)/(-2*tl)))[0]));
-        for Kvali in range(len(Kvals)):
+        # iter over E 
+        rhoJvals = np.linspace(0.5,5.0,myxvals);
+        Evals = Jval*Jval/(rhoJvals*rhoJvals*np.pi*np.pi*tl);
+        print(Evals)
+        print("longest NB = ",int((kNBvals[-1]/np.arccos( (Evals-2*tl)/(-2*tl)))[-1]));
+        assert False
+        for Evali in range(len(Evals)):
 
             # energy
-            Kval = Kvals[Kvali]; # Kval > 0 always, what I call K in paper
-            Energy = Kval - 2*tl; # -2t < Energy < 2t, what I call E in paper
+            Eval = Evals[Evali]; # Eval > 0 always, what I call K in paper
+            Energy = Eval - 2*tl; # -2t < Energy < 2t, what I call E in paper
             k_rho = np.arccos(Energy/(-2*tl)); # k corresponding to fixed \rho J a
 
             # set barrier distance
@@ -216,7 +218,7 @@ if True: # incident kinetic energy on the x axis
             hblocks[0:NC] += hblocks_cicc;
             hblocks[-1] += VB*np.eye(n_loc_dof);
             tnnn = np.zeros_like(tnn)[:-1]; # no next nearest neighbor hopping
-            if(Kvali == 0 and NBvali == 0): print("\nhblocks = \n",np.real(hblocks));
+            if(Evali == 0 and NBvali == 0): print("\nhblocks = \n",np.real(hblocks));
 
             # iter over sources
             for sourcei in range(n_loc_dof):
@@ -231,25 +233,25 @@ if True: # incident kinetic energy on the x axis
                 
                 # get R, T coefs
                 Rdum, Tdum = wfm.kernel(hblocks, tnn, tnnn, tl, Energy , source, all_debug = False);
-                Rvals[sourcei,:,Kvali,NBvali] = Rdum;
+                Rvals[sourcei,:,Evali,NBvali] = Rdum;
             #### end loop over sourcei
         #### end loop over E
 
-        # determine K*, ie x val where the SWAP happens
-        Kstar = Kvals[np.argmin(Rvals[1,1,:,NBvali])];
+        # determine rhoJa*
+        rhoJstar = rhoJvals[np.argmin(Rvals[1,1,:,NBvali])];
              
         # plot R_\sigma vs NBvals
         elecspin = 1;
         for sourcei in range(n_mol_dof):
             for sigmai in range(n_mol_dof):
-                axes[sourcei,sigmai].plot(Kvals, Rvals[4*elecspin+sourcei,4*elecspin+sigmai,:,NBvali], color=mycolors[NBvali], marker=mymarkers[1+NBvali], markevery=mymarkevery, linewidth=mylinewidth);
+                axes[sourcei,sigmai].plot(rhoJvals, Rvals[4*elecspin+sourcei,4*elecspin+sigmai,:,NBvali], color=mycolors[NBvali], marker=mymarkers[1+NBvali], markevery=mymarkevery, linewidth=mylinewidth);
                 #if(sourcei == sigmai): axes[sourcei,sigmai].axvline(rhoJstar, color=mycolors[NBvali]);
                 axes[sourcei,sigmai].set_title("$R("+str(ylabels[sourcei])+"\\rightarrow"+str(ylabels[sigmai])+")$");
                 axes[sourcei,sigmai].set_ylim(-0.1,1.1);
                 axes[sourcei,sigmai].axhline(0.0,color='lightgray',linestyle='dashed');
                 axes[sourcei,sigmai].axhline(1.0,color='lightgray',linestyle='dashed');
-                axes[-1,sigmai].set_xlabel('$K_i/t$',fontsize=myfontsize);
-                axes[-1,sigmai].set_xscale('log', subs = []);
+                axes[-1,sigmai].set_xlim(0.5,5.0);
+                axes[-1,sigmai].set_xlabel('$\\rho Ja$',fontsize=myfontsize);
                 #axes[sourcei,-1].plot(kNBvals/np.pi, np.sum(Rvals[4*elecspin+sourcei,4*elecspin+sigmai,:,NBvali],axis=0), color='red');
                     
     # show
@@ -262,4 +264,7 @@ if True: # incident kinetic energy on the x axis
     #np.savetxt(fname+".txt", kNBvals, header="[tl, tp, JK, J12, Dval, myspinS, n_loc_dof] =\n"+str(param_vals)+"\nkNBvals =");
     #np.save(fname+"_x", rhoJvals);
     #np.save(fname, Rvals);
+
+
+
 
