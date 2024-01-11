55c30cfcc63c37ef18c9da895b1f5c772c0d3cd8
Christian Bunker <cpbunker@login9.ufhpc>
2024-01-11 16:15:23 -0500

    first dimer results

--- run_stt_ed.py
+++ run_stt_ed.py
@@ -19,7 +19,7 @@ import sys
 #####################################################################
 #### util functions
 
-def snapshot(state,nsites,time, the_sites,plot=False):
+def snapshot(state,the_H,nsites,time, the_sites,plot=False):
     '''
     '''
 
@@ -36,8 +36,10 @@ def snapshot(state,nsites,time, the_sites,plot=False):
 
     # printout
     print("Time = {:.2f}".format(time));
-    print("Site {:.0f} <Sz> (ED) = {:.6f}".format(the_sites[0],szs[the_sites[0]])); 
-    print("Site {:.0f} <Sz> (ED) = {:.6f}".format(the_sites[1],szs[the_sites[1]]));
+    print("Total energy (ED) = {:.6f}".format(np.dot(np.conj(state),np.matmul(the_H, state))));
+    for sitei in range(len(the_sites)):
+        print("Site {:.0f} <n> (ED) = {:.6f}".format(the_sites[sitei],occs[sitei])); 
+        print("Site {:.0f} <Sz> (ED) = {:.6f}".format(the_sites[sitei],szs[sitei]));
     print("Summed occ (ED) = {:.6f}".format(np.sum(occs)));
     print("Summed sz (ED) = {:.6f}".format(np.sum(szs)));
     
@@ -66,10 +68,10 @@ def main(ham, nsites, init_state, time_snap, time_N, the_sites):
 
     # time prop with observables
     state = np.copy(init_state);
-    snapshot(state,nsites,0.0, the_sites);
+    snapshot(state,ham,nsites,0.0, the_sites);
     for time_stepi in range(1,time_N+1):
         state = tdfci.propagator(state, time_snap, eigvals, eigvecs);
-        snapshot(state,nsites,time_stepi*time_snap, the_sites);
+        snapshot(state,ham,nsites,time_stepi*time_snap, the_sites);
 
     return;
         
@@ -180,7 +182,7 @@ elif(case=="Ne1_NFM2"): # 2 impurity SWAP
                         [shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,trian_H,squar_H,trian_H],
                         [shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,trian_H,squar_H]]);
 
-    # operators (4d)
+    # impurity spin operators (4d)
     star_z1 = (1/2)*np.array([[1, 0, 0, 0, 0, 0, 0, 0],
                         [0, 1, 0, 0, 0, 0, 0, 0],
                         [0, 0,-1, 0, 0, 0, 0, 0],
@@ -218,22 +220,46 @@ elif(case=="Ne1_NFM2"): # 2 impurity SWAP
                         [shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,star_z2,shape_0],
                         [shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,shape_0,star_z2]]);
 
+    # site occ operators (4d)
+    nj0_4d = np.zeros_like(Sz1_4d);
+    nj0_4d[0,0] = np.eye(n_loc_dof);
+    nj1_4d = np.zeros_like(Sz1_4d);
+    nj1_4d[1,1] = np.eye(n_loc_dof);
+    nj7_4d = np.zeros_like(Sz1_4d);
+    nj7_4d[7,7] = np.eye(n_loc_dof);
+    nj8_4d = np.zeros_like(Sz1_4d);
+    nj8_4d[8,8] = np.eye(n_loc_dof);
+    
     # convert all to 2d
     H = utils.mat_4d_to_2d(H_4d);
-    sz_op_kwarg = np.array([utils.mat_4d_to_2d(Sz1_4d),utils.mat_4d_to_2d(Sz2_4d)]);
-    occ_op_kwarg = np.copy(sz_op_kwarg); # don't care about occ here
+    # we need sites 0,1,4,5,7,8
+    zero_filler = utils.mat_4d_to_2d(np.zeros_like(Sz1_4d));
+    sz_op_kwarg = np.array([zero_filler,zero_filler,utils.mat_4d_to_2d(Sz1_4d),utils.mat_4d_to_2d(Sz2_4d),zero_filler,zero_filler]);
+    occ_op_kwarg = np.array([utils.mat_4d_to_2d(nj0_4d),utils.mat_4d_to_2d(nj1_4d),zero_filler,zero_filler,utils.mat_4d_to_2d(nj7_4d),utils.mat_4d_to_2d(nj8_4d)]);
+
+    # treat j/d as same
+    occ_op_kwarg = [];
+    sz_op_kwarg = [];
+    for jdindex in range(L):
+        nj_4d = np.zeros_like(Sz1_4d);
+        nj_4d[jdindex,jdindex] = np.eye(n_loc_dof);
+        occ_op_kwarg.append(utils.mat_4d_to_2d(nj_4d));
+        szd_4d = np.zeros_like(Sz1_4d);
+        if(jdindex == 4):
+            szd_4d = np.copy(Sz1_4d);
+        elif(jdindex == 5):
+            szd_4d = np.copy(Sz2_4d);
+        sz_op_kwarg.append(utils.mat_4d_to_2d(szd_4d));
+    
 
     # run
     psii = np.zeros((L*n_loc_dof,),dtype=float);
-    psii[3] = 1.0; # j=0, Sze=up, Sz1=Sz2=down
+    psii[3] = 1.0 #4.95864938e-01 # j=0, Sze=up, Sz1=Sz2=down
+    psii[3+n_loc_dof] = 1.0 #5.03992770e-01  # j=1, Sze=up, Sz1=Sz2=down
+    psii = psii/np.sqrt(np.dot(np.conj(psii),psii));
     print(">>> H =\n",H);
     print(">>> psii = ",psii);
-    my_sites = [0,1]; # sites to print out
+    my_sites = np.arange(0,len(sz_op_kwarg));
     my_interval = 3.0; # how often to plot/printout
     my_updates = 2; # number of time interval repetitions
     main(H,len(sz_op_kwarg),psii,my_interval,my_updates,my_sites);
-
-
-
-
-
